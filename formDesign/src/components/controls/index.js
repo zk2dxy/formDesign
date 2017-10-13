99915cdaba5eb0f85385322f2c246e808960ad56
pangyy <1430632864@qq.com>
2017-10-13 09:01:25 +0800

    merge index.js

--- formDesign/src/components/controls/index.js
+++ formDesign/src/components/controls/index.js
@@ -4,13 +4,15 @@ import ControlConfigPyy from '@/components/base/ControlConfigPyy'
 import CIcon from '@/components/base/CIcon'
 import CValidate from '@/components/base/CValidate'
 import CInput from '@/components/controls/CInput'
+import CRadio from '@/components/controls/CRadio'
 
 const components = [
   ControlConfig,
   ControlConfigPyy,
   CIcon,
   CValidate,
-  CInput
+  CInput,
+  CRadio
 ]
 
 export default {
