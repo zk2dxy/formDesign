c68eff74835469e9fc683d2668b47be937f607a9
宣亚玲 <429597016@qq.com>
2017-10-18 20:17:30 +0800

    card卡片(分支)

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -8,6 +8,7 @@ import CValidate from '@/components/base/CValidate'
 import CInput from '@/components/controls/CInput'
 import CLayout from '@/components/layout/CLayout'
 import CCollapse from '@/components/controls/CCollapse'
+import CCard from '@/components/controls/CCard'
 import CRadio from '@/components/controls/CRadio'
 import CCheckbox from '@/components/controls/CCheckbox'
 import CSelect from '@/components/controls/CSelect'
@@ -21,6 +22,7 @@ const components = [
   CIcon,
   CLayout,
   CCollapse,
+  CCard,
   CValidate,
   CInput,
   CRadio,
